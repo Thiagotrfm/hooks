7bf634adb7804d3071fea6c2ebd3464d476de5b1
Thiagotrfm <thiago.felix@work.co>
2020-02-14 10:15:45 -0300

    Adding mission list request

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,27 @@ import './App.css';
 import axios from 'axios';
 import MissionItem, { Mission } from './Components/Mission';
 
-interface MissionList {
-  data: Array<Mission>
-}
 
 const App = () => {
   const [missionList, setMissionList] = useState([])
 
   useEffect(() => {
-    axios.request<Array<Mission>>({
-      url: 'https://api.spacexdata.com/v3/missions',
-    }).then((response) => {
-      console.log(response)
-    })
+    async function fetchData() {
+      const response = await axios.get('https://api.spacexdata.com/v3/missions')
+      const { data } = response
+
+      setMissionList(data)
+    }
+    fetchData();
   }, [])
 
   return (
     <div className="App">
       {
-        missionList.map((mission: Mission) => {
+        missionList.map((mission: Mission, index: number) => {
           return(
             <MissionItem
+              key={index}
               mission={mission}
             />
           )
